f2f79e7b0518ae1a0c03c61ac77744bbea0d150a
sahiraltaf <sahiraltaf69@gmail.com>
2024-11-06 00:54:02 +0500

    add to cart

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+use Session;
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return view('products', compact('products'));
+    }
+  
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function cart()
+    {
+        return view('cart');
+    }
+  
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function addToCart($id)
+    {
+        $product = Product::findOrFail($id);
+        $cart = session()->get('cart', []);
+        
+        if(isset($cart[$id])) {
+            $cart[$id]['quantity']++;
+        } else {
+            $cart[$id] = [
+                "name" => $product->name,
+                "quantity" => 1,
+                "price" => $product->price,
+                "image" => $product->image
+            ];
+        }
+  
+        session()->put('cart', $cart);
+
+        return redirect()->back()->with('success', 'Product added to cart successfully!');
+    }
+  
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function update(Request $request)
+    {
+        if($request->id & $request->quantity){
+            $cart = Session::get('cart');
+            $cart[$request->id]['quantity'] = (int)$request->quantity;
+            Session::put('cart', $cart);
+            // dd($cart[$request->id]['quantity'],$request->quantity, Session::get('cart'));
+            session()->flash('success', 'Cart updated successfully');
+        }
+    }
+  
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function remove(Request $request)
+    {
+        if($request->id) {
+            $cart = session()->get('cart');
+            if(isset($cart[$request->id])) {
+                unset($cart[$request->id]);
+                session()->put('cart', $cart);
+            }
+            session()->flash('success', 'Product removed successfully');
+        }
+    }
+}
---
+++ resources/views/products.blade.php
@@ -0,0 +1,53 @@
+@extends('layout')
+   
+@section('content') 
+<div class="row mt-4">
+    @foreach($products as $product)
+        <div class="col-md-3">
+            <div class="card text-center">
+                <img src="{{ $product->image }}" alt="" class="card-img-top">
+                <div class="caption card-body">
+                    <h4>{{ $product->name }}</h4>
+                    <p>{{ $product->description }}</p>
+                    <p><strong>Price: </strong> $ {{ $product->price }}</p>
+                    <a href="{{ route('add.to.cart', $product->id) }}" class="btn btn-warning btn-block text-center" role="button">Add to cart</a>
+
+                    <!-- <a href="javascript:void(0);" class="btn btn-warning btn-block text-center add-to-cart" role="button"  data-product-id="{{ $product->id }}">Add to cart</a> -->
+                
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
+
+@section('scripts')
+<script>
+    $(document).ready(function() {
+        $('.add-to-cart').on('click', function() {
+            var productId = $(this).data('product-id');
+            // alert(productId);
+            // Send AJAX request
+            $.ajax({
+                url: "{{ route('add.to.cart', '') }}/" + productId,
+                type: 'GET',
+                // data: {
+                //     _token: "{{ csrf_token() }}", // CSRF token for protection
+                // },
+                success: function(response) {
+                    // Show a success message
+                    console.log(response.message);
+                    alert(response.message);
+                    debugger;
+                    // Optionally update the cart count on the page
+                    // $('#cart-count-number').text(response.cartCount);
+                },
+                error: function(xhr, status, error) {
+                    alert('An error occurred: ' + xhr.responseJSON.message);
+                }
+            });
+        });
+    });
+</script>
+@endsection
+
+@endsection
